8dc525b3b264814e4d80de3b4fd613439cdfe3d7
Vitor Pereira <chronosmiki@users.noreply.github.com>
2018-08-29 23:37:20 -0300

    First commit
    Ler/Escrever um XML

--- Aux SaaS/Form1.Designer.cs
+++ Aux SaaS/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Aux_SaaS
 {
-    partial class Form1
+    partial class auxsaas
     {
         /// <summary>
         /// Variável de designer necessária.
@@ -28,13 +28,47 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnLer = new System.Windows.Forms.Button();
+            this.btnEscrever = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnLer
+            // 
+            this.btnLer.Location = new System.Drawing.Point(713, 389);
+            this.btnLer.Name = "btnLer";
+            this.btnLer.Size = new System.Drawing.Size(75, 23);
+            this.btnLer.TabIndex = 0;
+            this.btnLer.Text = "Ler";
+            this.btnLer.UseVisualStyleBackColor = true;
+            this.btnLer.Click += new System.EventHandler(this.btnRodar_Click);
+            // 
+            // btnEscrever
+            // 
+            this.btnEscrever.Location = new System.Drawing.Point(632, 389);
+            this.btnEscrever.Name = "btnEscrever";
+            this.btnEscrever.Size = new System.Drawing.Size(75, 23);
+            this.btnEscrever.TabIndex = 1;
+            this.btnEscrever.Text = "Escrever";
+            this.btnEscrever.UseVisualStyleBackColor = true;
+            this.btnEscrever.Click += new System.EventHandler(this.btnEscrever_Click);
+            // 
+            // auxsaas
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Controls.Add(this.btnEscrever);
+            this.Controls.Add(this.btnLer);
+            this.Name = "auxsaas";
+            this.Text = "Aux SaaS";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnLer;
+        private System.Windows.Forms.Button btnEscrever;
     }
 }
 
--- Aux SaaS/Form1.cs
+++ Aux SaaS/Form1.cs
@@ -7,14 +7,46 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+using System.Xml;
 namespace Aux_SaaS
 {
-    public partial class Form1 : Form
+    public partial class auxsaas : Form
     {
-        public Form1()
+        public auxsaas()
         {
             InitializeComponent();
         }
+
+        private void btnRodar_Click(object sender, EventArgs e)
+        {
+            XmlReader reader = XmlReader.Create("C:/ProgramData/TARGIT/ANTServer/VFS/Personal/SYS/Settings/Preferences.xml");
+
+            while (reader.Read())
+            {
+                if (reader.NodeType == XmlNodeType.Element
+                && reader.Name == "startup")
+                {
+                    MessageBox.Show("document = " + reader.GetAttribute(0));
+                }
+            }
+        }
+
+        private void btnEscrever_Click(object sender, EventArgs e)
+        {
+            XmlDocument xdoc = new XmlDocument();
+
+            xdoc.Load("C:/ProgramData/TARGIT/ANTServer/VFS/Personal/SYS/Settings/Preferences.xml");
+
+            XmlNode xnode = xdoc.SelectSingleNode("/userpreferences/startup");
+
+            MessageBox.Show(xnode.Value);
+
+            xnode.Attributes[0].Value = "vfs://Global/Analysis Examples/1.1 Sales Performance.xview";
+
+            xdoc.Save("C:/ProgramData/TARGIT/ANTServer/VFS/Personal/SYS/Settings/Preferences.xml");
+        }
     }
 }
+
+
+
